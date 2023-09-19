a9baff0e024abd37351a5cdb2e4509455b3bb569
Constenla Nicolas <constinic@gmail.com>
2023-09-19 12:56:24 -0300

    fixed steer angle at high speed

--- Codigo/Car scripts/CarController.cs
+++ Codigo/Car scripts/CarController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-
     public WheelScript[] wheels;
-    public float wheelBase;
-    public float rearTrack;
-    public float turnRadius;
+    private const float  wheelBase=2.8f;
+    private const float rearTrack= 1.5748f;
+    private const float turnRadius=5.85216f;
+    public float minSpeedForMaxSteering;
     public float steerInput;
     [SerializeField] private  float ackermanAngleLeft;
    [SerializeField] private  float ackermanAngleRight;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,30 +24,39 @@ public class CarController : MonoBehaviour
         if (steerInput>0) //girando derecha
         {
             ackermanAngleLeft=Mathf.Rad2Deg * Mathf.Atan(wheelBase/(turnRadius+(rearTrack/2)))*steerInput;
-            
+
             ackermanAngleRight=Mathf.Rad2Deg * Mathf.Atan(wheelBase/(turnRadius-(rearTrack/2)))*steerInput;
-        }else if (steerInput<0) //girando izquierda 
+
+
+        }else if (steerInput<0) //girando izquierda
         {
             ackermanAngleLeft=Mathf.Rad2Deg * Mathf.Atan(wheelBase/(turnRadius-(rearTrack/2)))*steerInput;
-            
+
             ackermanAngleRight=Mathf.Rad2Deg * Mathf.Atan(wheelBase/(turnRadius+(rearTrack/2)))*steerInput;
-            
+
         }else   //no esta girando
         {
             ackermanAngleLeft=0;
             ackermanAngleRight=0;
         }
 
+ 
+        // Calculate individual wheel angles using the Ackermann steering formula
+        float frontLeftWheelAngle =  Mathf.Lerp(ackermanAngleLeft, 8f*steerInput, wheels[1].getSpeed() / minSpeedForMaxSteering);
+        float frontRightWheelAngle =  Mathf.Lerp(ackermanAngleRight, 8f*steerInput, wheels[0].getSpeed() / minSpeedForMaxSteering);
+
+        // Debug.Log(frontLeftWheelAngle +" , "+ frontRightWheelAngle);
+
         foreach (WheelScript w in wheels)
         {
             if (w.frontLeft)
             {
-                w.wheelAngle=ackermanAngleLeft;
+                w.setWheelAngle(frontLeftWheelAngle);
             }
-            
+
             if (w.frontRight)
             {
-                w.wheelAngle=ackermanAngleRight;
+                w.setWheelAngle(frontRightWheelAngle);
             }
         }
     }
--- Codigo/Car scripts/WheelScript.cs
+++ Codigo/Car scripts/WheelScript.cs
@@ -3,65 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WheelScript : MonoBehaviour
-{   public bool frontLeft, rearLeft, frontRight,rearRight;
+{
+
+
+    public bool frontLeft, rearLeft, frontRight,rearRight;
     private Rigidbody rb;
-    public float restLenght;
-    public float springTravel;
-    public float springStiffnes;
+    private const float restLenght=0.5f;
+    private const float springTravel=0.2f;
+    private const float wheelRadius=0.6f;
+  
     private float minLeght;
     private float maxLenght;
     private float springLenght;
-    public float wheelRadius;
+  
     private float springForce;
     private Vector3 suspensionForce;
     private float springVelocity;
     private float damperForce;
-    private float lastLenght;
-    public float damperStiffnes;
-    public float wheelAngle;
-    public float fixedAngle;
-    public float TimeToSteer; 
+    private float lastLenght; 
+    private float wheelAngle;
+    private float fixedAngle;
+
     private float fuerzaEnX;
     private float fuerzaEnY;
     private Vector3 localWheelVelocity;
+    private BoxCollider bc;
+
+    [Header("Valores modificables")]
+        public float  forceApplied; //en porcentaje
+        public float springStiffnes;
+        public float TimeToSteer;
+         public float damperStiffnes;
+        
     void Start()
     {   rb= this.transform.parent.gameObject.transform.parent.gameObject.GetComponent<Rigidbody>();
-        // Debug.DrawRay(transform.position, -transform.up, Color color = Color.white);
+         bc= this.transform.parent.gameObject.transform.parent.gameObject.transform.GetChild(2).GetComponent<BoxCollider>();
         minLeght= restLenght-springTravel;
-        maxLenght= restLenght+springTravel; 
+        maxLenght= restLenght+springTravel;
     }
 
       void Update() {
+     
         fixedAngle= Mathf.Lerp(fixedAngle, wheelAngle, TimeToSteer*Time.deltaTime);
         transform.localRotation= Quaternion.Euler(transform.localRotation.x, transform.localRotation.y +fixedAngle,transform.localRotation.z);
- 
+        Debug.DrawRay(transform.GetChild(0).transform.position, -transform.up *(maxLenght+wheelRadius),Color.white);
+
     }
     void FixedUpdate()
-    {   
-        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLenght+wheelRadius))
+    {
+  
+        
+        if (Physics.Raycast(transform.GetChild(0).transform.position, -transform.up, out RaycastHit hit, maxLenght+wheelRadius))
     {   lastLenght=springLenght;
         springLenght=hit.distance-wheelRadius;
         springLenght=Mathf.Clamp(springLenght, minLeght, maxLenght);
+
+
         springVelocity=(lastLenght- springLenght)/Time.fixedDeltaTime;
+        
         springForce= springStiffnes*(restLenght-springLenght);
         damperForce= damperStiffnes*springVelocity;
         suspensionForce= (springForce+damperForce)*transform.up;
-        
-        
+
+
         localWheelVelocity=transform.InverseTransformDirection(rb.GetPointVelocity(hit.point));
-     
 
-
-
-        fuerzaEnX= -Input.GetAxis("Vertical")*springForce;
+        fuerzaEnX= -Input.GetAxis("Vertical")*springForce*(forceApplied/100);
         fuerzaEnY= localWheelVelocity.x *springForce;
+        // fuerzaEnY=0;
 
-       
+
 
 
            rb.AddForceAtPosition(suspensionForce+ (fuerzaEnX*transform.forward)+ (fuerzaEnY*-transform.right), hit.point);
-            Debug.Log("velocidad= "+suspensionForce+ (fuerzaEnX*transform.forward)+ (fuerzaEnY*-transform.right));
+     }
+     Debug.Log("current velocity "+rb.velocity.magnitude*3.6f);
+
     }
 
+    public void setWheelAngle(float aux){
+        this.wheelAngle=aux;
+    }
+    public float getSpeed(){
+        return rb.velocity.magnitude*3.6f;
     }
 }
